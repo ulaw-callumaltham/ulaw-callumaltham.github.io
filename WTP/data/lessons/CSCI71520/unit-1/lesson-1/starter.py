9cce7f2bf0819fd13e7d470f0dbbd0750a941ad9
Callum Altham <callum.altham@law.ac.uk>
2026-04-25 18:02:43 +0100

    gitignore and lesson update

---
+++ WTP/data/lessons/CSCI71520/unit-1/lesson-1/starter.py
@@ -0,0 +1,16 @@
+"""
+Python Introduction Starter File
+==================================
+This is a basic Python starter template for beginners.
+"""
+
+# TODO: Write your code below this line
+
+
+def main():
+    """Main function - entry point of the program."""
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
